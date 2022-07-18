73ca82d944ec53f9b16b87f86066460b29d86aea
priyankghosh <priyankghosh@hotmail.com>
2022-07-18 11:39:05 +0530

    Pics updated 2.0

--- RestaurantAPI/RestaurantAPI/Controllers/AdminController.cs
+++ RestaurantAPI/RestaurantAPI/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using RestaurantAPI.Models;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace RestaurantAPI.Controllers
@@ -64,5 +65,57 @@ namespace RestaurantAPI.Controllers
 
             return CreatedAtAction("GetAdminUsers", new { id = admin.AdminId }, admin);
         }
+
+        // PUT: api/Admin/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAdminUsers(int id, AdminUser admin)
+        {
+            if (id != admin.AdminId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(admin).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AdminExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Admin/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAdminUsers(int id)
+        {
+            var admin = await _context.AdminUsers.FindAsync(id);
+            if (admin == null)
+            {
+                return NotFound();
+            }
+
+            _context.AdminUsers.Remove(admin);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AdminExists(int id)
+        {
+            return _context.AdminUsers.Any(e => e.AdminId == id);
+        }
+
     }
 }
